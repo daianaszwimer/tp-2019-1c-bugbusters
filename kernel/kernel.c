a595bcfd0cb0eeaabfc329ff26a74c7ca753db4b
Daiana Szwimer <daiu.szwimer@gmail.com>
2019-05-12 14:37:11 -0300

    sacar comentarios

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -137,19 +137,10 @@ void liberarMemoria(void) {
 
 void enviarMensajeAMemoria(cod_request codRequest, char* mensaje) {
 	t_paquete* paqueteRecibido;
-	//t_paquete* paquete;
-	//sem_wait(&semEnviarMensajeAMemoria);
-	//printf("El mensaje es: %s \n", mensaje);
-	// El paquete tiene el cod_request y UN request completo
-	//paquete = armar_paquete(codRequest, mensaje);
-	//printf("Voy a enviar este cod: %d \n", paquete->palabraReservada);
-	//semaforos
 	enviar(codRequest, mensaje, conexionMemoria);
-	//free(paquete);
 	free(mensaje);
 	paqueteRecibido = recibir(conexionMemoria);
 	log_info(logger_KERNEL, "Recibi de memoria %s ", paqueteRecibido->request);
-	//sem_post(&semLeerDeConsola);
 }
 
 void procesarRun(char* mensaje) {
